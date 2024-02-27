383b7135a1188627a8cb9d8879bd95c70bbbd098
TN-99347 <TN-99347@usbd.com.bd>
2024-02-27 18:34:28 +0600

    repository pattern

--- JobPortal.API/JobPortal.API/obj/Debug/net8.0/JobPortal.API.AssemblyInfo.cs
+++ JobPortal.API/JobPortal.API/obj/Debug/net8.0/JobPortal.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobPortal.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9820e3cf1328a2485a2880bcf4932304d649da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobPortal.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobPortal.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
