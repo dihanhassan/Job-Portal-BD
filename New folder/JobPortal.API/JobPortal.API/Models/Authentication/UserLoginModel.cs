d050fc85704585a909e4a700707a851b0285ff41
TN-99347 <TN-99347@usbd.com.bd>
2024-03-18 13:03:19 +0600

    add custom log

---
+++ New folder/JobPortal.API/JobPortal.API/Models/Authentication/UserLoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobPortal.API.Models.Authentication
+{
+    public class UserLoginModel
+    {
+        public string UserID { get; set; } 
+        public string UserName { get; set; }
+       
+        public string UserPassword { get; set; }
+        public int UserType { get; set; }
+
+       
+
+
+
+    }
+}
---
+++ New folder/JobPortal.API/JobPortal.API/Services/Implementation/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using JobPortal.API.Models.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JobPortal.API.Services.Implementation
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task< string> AuthenticUser(UserLoginModel user)
+        {
+           
+            
+              return await  GenerateToken(user);
+           
+            
+        }
+        private async Task< string> GenerateToken(UserLoginModel user)
+        {
+
+            var security_key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credential = new SigningCredentials(security_key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], null,
+                    expires: DateTime.Now.AddMinutes(1),
+                    signingCredentials: credential
+
+                );
+
+            return  new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
